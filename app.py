0e1b352f0caebc704a1acb472442c6a570fc02b0
Zach Siegel <zsiegel92@gmail.com>
2019-05-20 13:55:29 -0700

    app setup

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return "Success! Welcome to this web app."
+
+# port = int(os.environ.get("PORT", 33507))
+# app.run(host='0.0.0.0', port=port)
+app.run()
