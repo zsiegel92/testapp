87f4f81eb0a7cb71b5bae63023ad682fa9a98e95
Zach Siegel <zsiegel92@gmail.com>
2019-05-20 14:01:36 -0700

    host, port

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def home():
 	return "Success! Welcome to this web app."
 
-# port = int(os.environ.get("PORT", 33507))
-# app.run(host='0.0.0.0', port=port)
-app.run()
+port = int(os.environ.get("PORT", 33507))
+app.run(host='0.0.0.0', port=port)
+# app.run()
